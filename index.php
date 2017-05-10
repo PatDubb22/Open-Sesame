d9353c1100574c0a4209cf84719211ad6132f1c6
Patricia Wheeler <pgwheeler@cox.net>
2017-05-10 23:42:22 +0000

    open sesame

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php   
+    
+        $password = "whatever";
+        $userpassword = $_GET ["password"];
+        
+            if ( $userpassword =="whatever") {
+                echo "Welcome back!";
+            } else {
+            
+               echo  "Password is incorrect please try again";
+            }
+    ?>
+   
